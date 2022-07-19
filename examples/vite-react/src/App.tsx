7effcbf81a9a98edf0d56be55c3b19f078c4dc58
cydro <cmrnfaith@gmail.com>
2022-07-18 18:11:40 -0600

    updated push

--- examples/vite-react/src/App.tsx
+++ examples/vite-react/src/App.tsx
@@ -1,9 +1,17 @@
 import { useAccount } from 'wagmi'
 
 import { Account, Connect, NetworkSwitcher } from './components'
+import { useEffect } from 'react'
 
 export function App() {
-  const { isConnected } = useAccount()
+  const { isConnected, address } = useAccount()
+
+ useEffect(() => {
+    console.log('test')
+
+    window.location.replace('unitydl://mylink/?address=' + address)
+  }, [address])
+  
 
   return (
     <>
